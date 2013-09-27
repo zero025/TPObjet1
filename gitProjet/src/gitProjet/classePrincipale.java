46c3943ca6bb27fe24d5d3a9c2d2959783329062
zero025 <brunt85@hotmail.fr>
2013-09-27 21:47:38 +0200

    D'autres fichiers du projet

--- gitProjet/src/gitProjet/classePrincipale.java
+++ gitProjet/src/gitProjet/classePrincipale.java
@@ -1,10 +1,18 @@
 package gitProjet;
+import java.io.File;
 
+/**
+ * @date {date}
+ * @author Thomas Brunel
+ * 
+ */
 public class classePrincipale {
 
+	/**
+	 * 
+	 * @param args Arguments de la commande.
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 	}
-
 }
