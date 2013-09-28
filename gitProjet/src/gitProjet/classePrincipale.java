372154566b2fd625c8d6716298111db16b6aec87
TBrunel <thomas.brunel@eleves.ec-nantes.fr>
2013-09-28 17:49:23 +0200

    Nouveau commit depuis Linux : commmentaires dans le code principal

--- gitProjet/src/gitProjet/classePrincipale.java
+++ gitProjet/src/gitProjet/classePrincipale.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 /**
  * @date {date}
- * @author Thomas Brunel
+ * @authors Thomas Brunel et Gabriel Guilbart
  * 
  */
 public class classePrincipale {
@@ -14,12 +14,17 @@ public class classePrincipale {
 	 * @param args Arguments de la commande.
 	 */
 	public static void main(String[] args){
+		//Création de la fenêtre :				
 		JFrame fenetre = new JFrame();
 		fenetre.setSize(600,600);
 		fenetre.setTitle("Lena");
 		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//Création du panneau qui contiendra l'image :		
 		Panneau pan = new Panneau();
 		fenetre.setContentPane(pan);
+
+		//Affichage de la fenêtre :
 		fenetre.setVisible(true);
 	}
 }
